0fd73c6dd43abdae9df475d988b0f09b19cbc54e
ogurchik228yt <149517275+ogurchik228yt@users.noreply.github.com>
2024-05-28 23:17:32 +0300

    Add files via upload

---
+++ 7/backend-connect-db/routes/users.js
@@ -0,0 +1,51 @@
+const usersRouter = require("express").Router();
+
+const {
+    findAllUsers,
+    createUser,
+    updateUser,
+    deleteUser,
+    checkEmptyNameAndEmailAndPassword,
+    checkEmptyNameAndEmail,
+    hashPassword
+} = require("../middlewares/users");
+
+const {
+    sendAllUsers,
+    sendUserCreated,
+    sendUserUpdated,
+    sendUserDeleted,
+    sendMe
+} = require("../controllers/users");
+
+const { checkAuth } = require("../middlewares/auth");
+
+usersRouter.get("/users", findAllUsers, sendAllUsers);
+usersRouter.post(
+    "/users",
+    findAllUsers,
+    //checkIsUserExists,
+    checkEmptyNameAndEmailAndPassword,
+    hashPassword,
+    checkAuth,
+    createUser,
+    sendUserCreated
+);
+usersRouter.put(
+    "/users/:id",
+    checkEmptyNameAndEmail,
+    checkAuth,
+    updateUser,
+    sendUserUpdated
+);
+
+usersRouter.delete(
+    "/games/:id", 
+    checkAuth,
+    deleteUser,
+    sendUserDeleted 
+);
+
+usersRouter.get("/me", checkAuth, sendMe);
+
+module.exports = usersRouter;

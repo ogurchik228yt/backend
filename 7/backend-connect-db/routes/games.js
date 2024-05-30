bfa56b5e035ab236fea71eaa49665fafa854ac6d
ogurchik228yt <149517275+ogurchik228yt@users.noreply.github.com>
2024-05-30 17:21:21 +0300

    Add files via upload

---
+++ 7/backend-connect-db/routes/games.js
@@ -0,0 +1,54 @@
+const gamesRouter = require("express").Router();
+
+const {
+    findAllGames,
+    createGame,
+    updateGame,
+    deleteGame,
+    checkEmptyFields,
+    checkIfUsersAreSafe,
+    checkIfCategoriesAvaliable,
+    checkIsGameExists,
+    checkIsVoteRequest,
+    findGameById
+} = require("../middlewares/games");
+
+const {
+    sendAllGames,
+    sendGameCreated,
+    sendGameUpdated,
+    sendGameDeleted,
+    sendGameById
+} = require("../controllers/games");
+
+const { checkAuth } = require("../middlewares/auth");
+
+//gamesRouter.get("/games/:id", findGameById, sendGameById);
+gamesRouter.get("/games", findAllGames, sendAllGames);
+gamesRouter.post(
+    "/games",
+    findAllGames,
+    checkIsGameExists,
+    checkEmptyFields,
+    checkAuth,
+    createGame,
+    sendGameCreated
+); 
+gamesRouter.put(
+    "/games/:id", 
+    checkIsVoteRequest,
+    checkIfUsersAreSafe,
+    checkIfCategoriesAvaliable,
+    checkEmptyFields,
+    checkAuth,
+    updateGame,
+    sendGameUpdated
+  );
+gamesRouter.delete(
+    "/games/:id",
+    checkAuth, 
+    deleteGame,
+    sendGameDeleted 
+  );
+
+module.exports = gamesRouter;

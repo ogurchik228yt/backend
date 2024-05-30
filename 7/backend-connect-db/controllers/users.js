f16ba3ae4026473abf8bc38b2029fe6523c3a61f
ogurchik228yt <149517275+ogurchik228yt@users.noreply.github.com>
2024-05-30 13:51:22 +0300

    Update users.js

--- 7/backend-connect-db/controllers/users.js
+++ 7/backend-connect-db/controllers/users.js
@@ -10,7 +10,7 @@ const sendUserCreated = (req, res) => {
 
 const sendUserUpdated = (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    res.end({ message: "Пользователь обновлён" });
+    res.send(JSON.stringify(req.user));
 };
 
 const sendUserDeleted = (req, res) => {

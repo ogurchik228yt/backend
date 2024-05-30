026a48a3014acf45683b09a8f0bf127d49690f70
ogurchik228yt <149517275+ogurchik228yt@users.noreply.github.com>
2024-05-30 17:20:20 +0300

    Delete 7/backend-connect-db directory

--- 7/backend-connect-db/routes/pages.js
+++
@@ -1,9 +0,0 @@
-const pagesRouter = require("express").Router();
-
-const { checkCookiesJWT, checkAuth } = require("../middlewares/auth");
-const { sendIndex, sendDashboard } = require("../controllers/auth");
-
-pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
-pagesRouter.get("/", sendIndex);
-
-module.exports = pagesRouter;
